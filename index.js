5194088d1f6d9a97d913e48781278fa1a9c73982
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-18 06:58:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string===string.toLowerCase() && string==="hello")
+  let lowCase="hello"
+  if(string=lowCase)
+  if(string===string.toLowerCase())
   return "I can't hear you!"
 }
 
