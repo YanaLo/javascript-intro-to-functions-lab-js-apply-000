25a465b7e433283db7e233579c078118698b5087
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-18 06:48:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
 
 function sayHiToGrandma(string){
   if(string.toUpperCase()===string)
-  return "string.toUpperCase()"
+  return "YES INDEED!"
 }
 
 function sayHiToGrandma(string){
